be45231f8a6c7d38f3136ecd26f2bc06d5ebd33a
ernesthenry <henry38ernest@gmail.com>
2023-12-30 19:08:03 +0300

    Add lang client

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-
-
 llm = ChatOpenAI(openai_api_key="")
 llm.invoke("how can langsmith help with testing?")
 prompt = ChatPromptTemplate.from_messages([
